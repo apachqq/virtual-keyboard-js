db0951ce08b1bee948c5443cbfa5d0b91e9d9261
apachqq <work.tsyganov@mail.ru>
2022-02-26 11:48:39 +0300

    for-js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let keys = document.querySelectorAll('.key');
 let display = document.querySelector('.display');
 let clear = document.querySelector('.clear');
+
+for (let key of keys) {
+    key.onclick = function () {
+        display.textContent = display.textContent + key.textContent;
+    }
+}
